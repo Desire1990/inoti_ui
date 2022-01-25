a56c47fb9b1225a233c895d927a09e58326a5506
Desos <desnduwim26@gmail.com>
2022-01-25 14:04:53 +0200

    aprovision, depenses worked

---
+++ src/pages/Approvision.vue
@@ -0,0 +1,44 @@
+<template>
+    <div> 
+        
+        <div class="text-right">
+            <searchbar />
+             <button class="btn btn-primary mt-3" @click="toggleForm">Nouvel approvision</button>
+             <modale-depot v-bind:revele="revele" v-bind:toggleForm="toggleForm" />             
+            <depot-list class=" mt-3"/>
+        </div>
+    </div>
+</template>
+
+<script>
+import DepotList from '../components/ApprovisionList.vue'
+import ModaleDepot from '../components/ModalApprovision.vue'
+import searchbar from '../components/SearchBar.vue'
+export default {
+  components: { DepotList, ModaleDepot, searchbar },
+    data(){
+        return {
+           
+        revele : false
+        }
+    },
+    mounted() {
+    },
+    computed:{
+        header(){
+            return{
+                headers:{
+                    "Authorization" : `Bearer  ${this.$store.state.user.access }`
+
+                }
+            }
+        }
+    },
+    methods: {
+
+        toggleForm(){
+            this.revele = ! this.revele
+        }
+    }
+}
+</script>
---
+++ src/pages/Depenses.vue
@@ -0,0 +1,41 @@
+<template>
+    <div>        
+        <div class="text-right">
+            <searchbar />
+             <button class="btn btn-primary mt-3" @click="toggleForm">Nouvelle depense</button>
+             <ModalDepense v-if='new_depot' @close='close'/>             
+            <DepenseList  class=" mt-3"/>
+        </div>
+    </div>
+</template>
+<script>
+import DepenseList from '../components/DepenseList.vue'
+import ModalDepense from '../components/ModalDepense.vue'
+import searchbar from '../components/SearchBar.vue'
+export default {
+  components: { DepenseList, ModalDepense, searchbar },
+    data(){
+        return {           
+        new_depot : false
+        }
+    },
+    computed:{
+        header(){
+            return{
+                headers:{
+                    "Authorization" : `Bearer  ${this.$store.state.user.access }`
+
+                }
+            }
+        }
+    },
+    methods: {
+        close(){
+            this.new_depot=false
+        },
+        toggleForm(){
+            this.new_depot =true
+        }
+    }
+}
+</script>
