024083bb7b6cda2d1be669c996ab2a70dae0c9ba
Desos <desnduwim26@gmail.com>
2022-02-01 18:23:48 +0200

    validate/reject boutton on actions

--- src/components/DepenseList.vue
+++ src/components/DepenseList.vue
@@ -7,18 +7,23 @@
                 <th>MONTANT</th>
                 <th>DATE</th>
                 <th>MOTIF</th>
+                <th>STATUS</th>
                 <th>ACTIONS</th>
             </tr>
         </thead>
         <tbody>
-            <tr class="text-left" v-for="depens in depenses.results" :key="depens.id">
+            <tr class="text-left" v-for="depens in depenses.results" :key="depens.id" :class="classe[depens.validate]">
                 <td>{{ depens.id }}</td>
                 <td> {{ money(depens.montant)}}</td>
                 <td>{{datetime(depens.date)}}</td>
                 <td>{{(depens.motif)}}</td>
+                <td>{{(depens.validate)}}</td>
                 <td>
-                <button class="btn btn-success btn-sm ml-1" v-on:click="valider(depense)" v-if='$store.state.user.groups.includes("admin")'>
+                <button class="btn btn-success btn-sm ml-1" v-on:click.once="valider(depens)" v-if='$store.state.user.groups.includes("admin")'>
                     valider
+                </button> 
+                <button class="btn btn-success btn-sm ml-1" v-on:click.once="rejeter(depens)" v-if='$store.state.user.groups.includes("admin")'>
+                    rejeter
                 </button> 
                     <button v-else class="btn btn-danger btn-sm ml-1 " @click="DeleteDepot(depens)">
                     Delete
@@ -41,9 +46,13 @@ export default {
     data() {
         return {
             form:{
-                montant:'',
-                motif:''
+                validate:''
             },
+            classe:{
+                'Validé':'gold-star',
+                'Attente':'bronze-star',
+                'Rejeté':'bro-star'
+              },
             update:false,
             updatedepot:null,
             depenses : this.$store.state.depenses
@@ -106,7 +115,42 @@ export default {
                         return response
                     });
                 }
-            }  
+            },
+        valider(dep){
+            if (confirm('êtes-vous sur de vouloir valider le depense de ' + dep.montant)) {
+                axios.patch(this.$store.state.url+`/depense/${dep.id}`+'/',{
+                    validate:'Validé'
+                }, this.header)
+                .then(response => {
+                    const compte =(dep.account.montant_burundi)
+                    const fees = (dep.montant)
+                    const x = this.compte-this.fees
+                    console.log(this.x)
+                    axios.patch(this.$store.state.url+`/compte/`,{
+                        montant_burundi:this.x
+                        }, this.header)
+                    this.fetchData(dep.account.montant_burundi-=dep.montant)
+                    return response
+                }).catch(err => {
+                    console.error(err); 
+                })
+            }
+        },         
+        rejeter(dep){
+            if (confirm('êtes-vous sur de vouloir rejeter le depense de ' + dep.montant)) {
+                axios.patch(this.$store.state.url+`/depense/${dep.id}`+'/',{
+                    validate:'Rejeté'
+                }, this.header)
+                .then(response => {
+                    console.log(response.data)
+                    this.fetchData()
+                    return response
+                }).catch(err => {
+                    console.error(err); 
+                })
+            }
+
+        },          
         }
     }
 </script>
@@ -115,5 +159,17 @@ export default {
     background-color: #20c997;
     text-align: left;
     text-decoration-color: white;
+}
+.gold-star {
+  background-color: sandybrown;
+  color:white ;
+}
+.bronze-star {
+  background-color: #2d74e1;
+  color:white ;
+}
+.bro-star {
+  background-color: red ;
+  color:white ;
 }
 </style>
